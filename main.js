f65d9e98797868d93a9515a1e3feaea638d49a37
Tom Kane <45863735+thomasjkane67@users.noreply.github.com>
2019-03-02 21:15:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-var url = 'https://thomasjkane67.github.io/json/Books.json'
-var NetMap = new XMLHttpRequest();
-NetMap.open('GET', url);
-NetMap.onload = function() {
-  var NetMapData = JSON.parse(NetMap.responseText);
-  console.log(NetMapData[0]);
-};
-NetMap.send();
+var btn = document.getElementById("btn");
+    
+btn.addEventListener("click", function() {
+  var url = 'https://thomasjkane67.github.io/json/Books.json'
+  var NetMap = new XMLHttpRequest();
+  NetMap.open('GET', url);
+  NetMap.onload = function() {
+    var NetMapData = JSON.parse(NetMap.responseText);
+    console.log(NetMapData[0]);
+  };
+  NetMap.send();
+});
+                     
+                  
+
 
