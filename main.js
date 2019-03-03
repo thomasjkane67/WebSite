debfa84ac44481e2f802f1e3d841e908d67907ea
Tom Kane <45863735+thomasjkane67@users.noreply.github.com>
2019-03-02 20:54:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var url = 'https://thomasjkane67.github.io/json/'
 var myBooks = new XMLHttpRequest();
 myBooks.open('GET', url);
 myBooks.onliad = function() {
-  console.log(myBooks.responseText);
+  console.log(myBooks[0]);
 };
 myBooks.send();
 
